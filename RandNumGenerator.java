7b782960dc14705fc98761efc3c5c442380d1c39
Neil Opena <neil1114o@gmail.com>
2016-09-24 23:27:25 -0400

    changed range

--- RandNumGenerator.java
+++ RandNumGenerator.java
@@ -1,9 +1,11 @@
 
 public class RandNumGenerator {
-	public static void main(String [] args) {
-		int range = 40;      // Prints a number from 0 to max
+
+public static void main(String [] args) {
+		int range = 4000;      // Prints a number from 0 to max
 		int min = 10;
 		int num = (int) (Math.random() * range) + min;
 		System.out.println(num);
 	}
 }
+
