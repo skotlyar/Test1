43e8414c9993e1a6b153044335bcb8953162ca61
skotlyar <samuel.kotlyar@stonybrook.edu>
2016-09-24 23:06:22 -0400

    Uploaded file

---
+++ RandNumGenerator.java
@@ -0,0 +1,10 @@
+
+public class RandNumGenerator {
+	public static void main(String [] args) {
+		int range = 40;      // Prints a number from 0 to max
+		int min = 10;
+		int num = (int) (Math.random() * range) + min;
+		System.out.println(num);
+		//This is a change
+	}
+}
